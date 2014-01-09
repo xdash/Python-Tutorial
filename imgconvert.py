ad4d90338790097ee1e83f5a0a85f8ded6f9ae08
XDash <fanbingx@gmail.com>
2014-01-09 17:48:02 +0800

    允许文件名前后空格
    方便直接从Finder里拖拽到CommandLine的文件路径

--- imgconvert.py
+++ imgconvert.py
@@ -3,17 +3,17 @@
 from PIL import Image
 
 source_file_path = raw_input("What source file?")
-im = Image.open(source_file_path)
+im = Image.open(source_file_path.strip())
 convert_format = raw_input("What target format (png/jpg/gif)?")
 
 if (convert_format == "jpg"):
-		im.save(source_file_path + ".jpg");
-		print "Converted to " + source_file_path + ".jpg"
+		im.save(source_file_path.strip() + ".jpg");
+		print "Converted to " + source_file_path.strip() + ".jpg"
 elif (convert_format == "gif"):		
-		im.save(source_file_path + ".gif");
-		print "Converted to " + source_file_path + ".gif"
+		im.save(source_file_path.strip() + ".gif");
+		print "Converted to " + source_file_path.strip() + ".gif"
 elif (convert_format == "png"):
-		im.save(source_file_path + ".png");
-		print "Converted to " + source_file_path + ".png"
+		im.save(source_file_path.strip() + ".png");
+		print "Converted to " + source_file_path.strip() + ".png"
 else:
 		print "Only supports jpg/gif/png.";
